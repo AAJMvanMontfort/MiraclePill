55761088d8dfb9df774adc2d49d8ceb0ec839f4a
AAJM van Montfort <lout@macbook-pro-001-van-aajm-van-montfort.local>
2017-01-14 12:20:33 +0100

    Final version

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var successBtn: UIImageView!
+  
+    let states = ["Friesland", "Groningen", "Drente", "Overijssel", "Gelderland", "Flevoland", "Utrecht", "Noord-Holland", "Zuid-Holland", "Zeeland", "Noord-Brabant", "Limburg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.green
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +29,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func statePickerBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
 
+    @IBAction func buyNowBtnPressed(_ sender: Any) {
+        successBtn.isHidden = false
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        
+    }
 }
 
